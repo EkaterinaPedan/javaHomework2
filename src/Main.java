a3ce40111031c320c397f4240960144821fe1ebd
Ekaterina Pedan <kat.pedan@yandex.ru>
2024-05-25 20:27:20 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_789;
+        int amountPerMile = 20;
+
+        int bonusMile = amount / amountPerMile;
+
+        System.out.println(bonusMile);
+    }
+}
